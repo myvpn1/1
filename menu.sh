399fcbabb478e445216bd6df46e8c6478618ba35
myvpn1 <137178860+myvpn1@users.noreply.github.com>
2023-06-21 13:51:47 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -216,13 +216,8 @@ echo ""
 read -n 1 -s -r -p "Press any key to back on menu"
 menu
 }
-export sem=$( curl -s https://raw.githubusercontent.com/wunuit/test/main/versions)
-export pak=$( cat /home/.ver)
 IPVPS=$(curl -s ipinfo.io/ip )
 ISPVPS=$( curl -s ipinfo.io/org )
-export Server_URL="raw.githubusercontent.com/wunuit/test/main"
-License_Key=$(cat /etc/${Auther}/license.key)
-export Nama_Issued_License=$( curl -s https://${Server_URL}/validated-registered-license-key.txt | grep -w $License_Key | cut -d ' ' -f 7-100 | tr -d '\r' | tr -d '\r\n')
 clear
 echo -e "\033[0;34m┌─────────────────────────────────────────────────────┐${NC}"
 echo -e "\033[0;34m│                  ${BIWhite}${UWhite}Server Informations${NC}"
